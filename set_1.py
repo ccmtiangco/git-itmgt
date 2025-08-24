9d2ea1ea1dd3c502a23da8ebb96a8e5138e99d2b
ccmtiangco <carina.cristina.tiangco@student.ateneo.edu>
2025-08-24 23:35:48 +0800

    3rd attempt

--- set_1.py
+++ set_1.py
@@ -1,5 +1,5 @@
 def savings(gross_pay, tax_rate, expenses):
-    return ((gross_pay - int(gross_pay * tax_rate))- expenses)
+    return int((gross_pay - int(gross_pay * tax_rate))- expenses)
 def material_waste(total_material, material_units, num_jobs, job_consumption):
     return str(total_material - (num_jobs * job_consumption)) + str(material_units)
 def interest(principal, rate, periods):
